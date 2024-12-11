10be4fb8a1bec0c8a1efb6e616be4776d12bd082
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-12-11 01:16:37 +0100

    Dodaj pliki projektów.

---
+++ babeZeslalBog/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace babeZeslalBog
+{
+    internal class Program
+    {
+        static private bool czyJestBaba(string slowo)
+        {
+            string baba = "";
+            foreach (char c in slowo){
+                if (baba.Length % 2 == 0 && c == 'b')
+                    baba += c;
+                else if(baba.Length % 2 == 1 && c == 'a')
+                    baba += c;
+
+                if (baba == "baba")
+                    return true;
+            }
+
+            return false;
+        }
+        
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            List<bool> czyBaba = new List<bool>();
+
+            for(int i = 0; i < n; i++){
+                czyBaba.Add(czyJestBaba(Console.ReadLine()));
+            }
+
+            foreach(bool b in czyBaba)
+            {
+                Console.WriteLine(b ? "Tak" : "Nie");
+            }
+        }
+    }
+}
